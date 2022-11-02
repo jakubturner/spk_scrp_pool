7849dd1f865d98bc40e398ad6c1c46c143b61fd4
Jakub Turner <kuba.turner@gmail.com>
2022-11-02 11:51:36 +0100

    init app

---
+++ src/app.py
@@ -0,0 +1,45 @@
+import psycopg2
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+from database import config
+
+
+def insert_row(num_pool, num_sauna):
+    """ insert a new row into the visitors table """
+    sql = """insert into visitors (num_pool_visitors, num_sauna_visitors,date ) values (%s,%s,%s)"""
+
+    conn = None
+    try:
+        # read database configuration
+        params = config()
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the INSERT statement
+        cur.execute(sql, (num_pool, num_sauna, datetime.now()))
+        # get the generated id back
+        # commit the changes to the database
+        conn.commit()
+        # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+def app():
+    url = "https://www.sumperksportuje.cz/aquacentrum/kryty-bazen"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    swm_count = soup.find(id="pool-part-count")
+    sauna_count = soup.find(id="sauna-part-count")
+    swm_count_str = str(swm_count.string).split("/")
+    sauna_count_str = str(sauna_count.string).split("/")
+    num_ppl_in_swim = int(swm_count_str[0])
+    num_ppl_in_sauna = int(sauna_count_str[0])
+
+    return insert_row(num_ppl_in_swim,num_ppl_in_sauna)
---
+++ src/create_table.py
@@ -0,0 +1,34 @@
+from src.database import config
+import psycopg2
+
+
+def create_tables():
+    """ create tables in the PostgreSQL database"""
+    sql = '''CREATE TABLE VISITORS(
+       ID SERIAL UNIQUE NOT NULL,
+       NUM_POOL_VISITORS INTEGER,
+       NUM_SAUNA_VISITORS INTEGER,
+       DATE TIMESTAMP WITH TIME ZONE
+    )'''
+    conn = None
+    try:
+        # read the connection parameters
+        params = config()
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        # create table one by one
+        cur.execute(sql)
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+if __name__ == '__main__':
+    create_tables()
