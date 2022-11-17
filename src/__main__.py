7cdd39f5f4ef5034f6b2dce09636071f6628c127
jakubturner <kuba.turner@gmail.com>
2022-11-17 13:33:41 +0100

    refactor structure and cleand little bit

--- src/__main__.py
+++ src/__main__.py
@@ -1,4 +1,21 @@
-from app import app
+import asyncio
+
+from src.app import App
+
+
+async def run() -> None:
+    app = App()
+    await app.setup()
+
+    try:
+        await app.scrapper()
+    except KeyboardInterrupt:
+        pass
+
+
+def main() -> None:
+    asyncio.run(run())
+
 
 if __name__ == "__main__":
-    app()
+    main()
--- src/app.py
+++ src/app.py
@@ -5,41 +5,48 @@ from datetime import datetime
 from database import config
 
 
-def insert_row(num_pool, num_sauna):
-    """ insert a new row into the visitors table """
-    sql = """insert into visitors (num_pool_visitors, num_sauna_visitors,date ) values (%s,%s,%s)"""
+class App:
 
-    conn = None
-    try:
-        # read database configuration
-        params = config()
-        # connect to the PostgreSQL database
-        conn = psycopg2.connect(**params)
-        # create a new cursor
-        cur = conn.cursor()
-        # execute the INSERT statement
-        cur.execute(sql, (num_pool, num_sauna, datetime.now()))
-        # get the generated id back
-        # commit the changes to the database
-        conn.commit()
-        # close communication with the database
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
+    def __init__(self):
+        pass
 
+    async def setup(self):
+        pass
 
-def app():
-    url = "https://www.sumperksportuje.cz/aquacentrum/kryty-bazen"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    swm_count = soup.find(id="pool-part-count")
-    sauna_count = soup.find(id="sauna-part-count")
-    swm_count_str = str(swm_count.string).split("/")
-    sauna_count_str = str(sauna_count.string).split("/")
-    num_ppl_in_swim = int(swm_count_str[0])
-    num_ppl_in_sauna = int(sauna_count_str[0])
+    async def insert_row(self,num_pool, num_sauna):
+        """ insert a new row into the visitors table """
+        sql = """insert into visitors (num_pool_visitors, num_sauna_visitors,date ) values (%s,%s,%s)"""
 
-    return insert_row(num_ppl_in_swim,num_ppl_in_sauna)
+        conn = None
+        try:
+            # read database configuration
+            params = config()
+            # connect to the PostgreSQL database
+            conn = psycopg2.connect(**params)
+            # create a new cursor
+            cur = conn.cursor()
+            # execute the INSERT statement
+            cur.execute(sql, (num_pool, num_sauna, datetime.now()))
+            # get the generated id back
+            # commit the changes to the database
+            conn.commit()
+            # close communication with the database
+            cur.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
+
+    async def scrapper(self):
+        url = "https://www.sumperksportuje.cz/aquacentrum/kryty-bazen"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        swm_count = soup.find(id="pool-part-count")
+        sauna_count = soup.find(id="sauna-part-count")
+        swm_count_str = str(swm_count.string).split("/")
+        sauna_count_str = str(sauna_count.string).split("/")
+        num_ppl_in_swim = int(swm_count_str[0])
+        num_ppl_in_sauna = int(sauna_count_str[0])
+
+        await self.insert_row(num_ppl_in_swim, num_ppl_in_sauna)
